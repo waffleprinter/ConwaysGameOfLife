9b0734fddba49d1a1537ac6359d8879ed8c05a09
waffleprinter <dwamsie@gmail.com>
2024-10-08 00:24:23 -0400

    Fixed CMakeCache.txt not adding the dlls to build, added basic window to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Window
+    sf::RenderWindow window(sf::VideoMode(720, 480), "SFML Test", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event event;
+
+    // Game loop
+    while (window.isOpen()) {
+        // Event polling
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case (sf::Event::Closed):
+                    window.close();
+                    break;
+
+                case (sf::Event::KeyPressed):
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
+            }
+        }
+    }
+
+    // Update
+
+    // Render
+    window.clear(); // Clear old frame
+
+    // Draw game
+
+    window.display(); // Tells app that window is done drawing
+
+    // End of application
     return 0;
 }
