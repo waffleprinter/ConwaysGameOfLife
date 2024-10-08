8141c1863a5fe22a0c29adaddf9d44f06465ce72
waffleprinter <dwamsie@gmail.com>
2024-10-08 00:48:36 -0400

    Added test for cell, it works :thumbsup:

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include "Cell.h"
 
 int main() {
     // Window
     sf::RenderWindow window(sf::VideoMode(720, 480), "SFML Test", sf::Style::Titlebar | sf::Style::Close);
     sf::Event event;
+
+    Cell cell = Cell(false, sf::Vector2f(360, 240));
 
     // Game loop
     while (window.isOpen()) {
@@ -22,7 +25,13 @@ int main() {
             }
         }
 
+        // Update
+        cell.toggle();
+
+        // Render
         window.clear();
+
+        cell.draw(window);
 
         window.display();
     }
