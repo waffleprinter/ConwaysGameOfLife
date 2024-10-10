b92e7241921d6c5ad52270fa6a2023ce40d55527
waffleprinter <dwamsie@gmail.com>
2024-10-10 19:59:30 -0400

    Created Game class

---
+++ headers/Game.h
@@ -0,0 +1,28 @@
+#ifndef CONWAYSGAMEOFLIFE_GAME_H
+#define CONWAYSGAMEOFLIFE_GAME_H
+
+#include <SFML/Graphics.hpp>
+#include "Board.h"
+
+class Game {
+private:
+    sf::RenderWindow window;
+    sf::VideoMode videoMode;
+    sf::Event event{};
+    Board board = Board(0, 0, 0, sf::Vector2f(0, 0));
+
+    void initWindow();
+    void initBoard();
+
+    void pollEvents();
+    void update();
+    void draw();
+
+public:
+    Game();
+
+    void run();
+};
+
+
+#endif //CONWAYSGAMEOFLIFE_GAME_H
---
+++ src/Game.cpp
@@ -0,0 +1,59 @@
+#include "Game.h"
+
+Game::Game() {
+    initWindow();
+    initBoard();
+}
+
+void Game::initWindow() {
+    videoMode.width = 720;
+    videoMode.height = 480;
+
+    window.create(videoMode, "Conway's Game of Life", sf::Style::Titlebar | sf::Style::Close);
+}
+
+void Game::initBoard() {
+    float cellSize = 5.0f;
+    float cellSizeWithPadding = cellSize + 1;
+    int rows = videoMode.height / cellSizeWithPadding;
+    int cols = videoMode.width / cellSizeWithPadding;
+
+    board = Board(cellSize, cols, rows, sf::Vector2f(0, 0));
+}
+
+void Game::pollEvents() {
+    while (window.pollEvent(event)) {
+        switch (event.type) {
+            case (sf::Event::Closed):
+                window.close();
+                break;
+
+            case (sf::Event::KeyPressed):
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close();
+                break;
+        }
+    }
+}
+
+void Game::update() {
+    board.update();
+}
+
+void Game::draw() {
+    window.clear(sf::Color::White);
+
+    board.draw(window);
+
+    window.display();
+}
+
+void Game::run() {
+    while (window.isOpen()) {
+        pollEvents();
+        update();
+        draw();
+    }
+}
+
+
